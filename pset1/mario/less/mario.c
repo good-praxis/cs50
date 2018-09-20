d06ac44755e365dbba5a04dc1212eecd28eec3a2
Darian Reck <hello@vulpinecat.com>
2018-09-20 09:52:56 +0000

    updated style to pass style50 on mario.c

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -3,22 +3,38 @@
 //As I am writing this code ahead of watching the lecture, I am using https://www.tutorialspoint.com/cprogramming to help me
 //with input methods and type conversion
 
-int main(void){
-    while(1) {
+int main(void)
+{
+
+    while (1)
+    {
+
         int startValue = -1;
-        int * input = &startValue;
-        while((*input < 0) || (*input > 23)) {
+        int *input = &startValue;
+        //This input is the easiest way I found it to be done, will see if this is how they want us to do it after the lecture (I don't think so)
+
+        //Leaving loop when the value is permitted
+        while ((*input < 0) || (*input > 23))
+        {
             printf("\nHeight: ");
             scanf("%d", input);
         }
 
-        for(int i = 0; i < *input; i++) {
-            for(int j = 0; (j < (*input - 1 - i)); j++) {
+        //entering print loop
+        for (int i = 0; i < *input; i++)
+        {
+            //printing empty space
+            for (int j = 0; (j < (*input - 1 - i)); j++)
+            {
                 printf(" ");
             }
-            for(int j = 0; j < i + 2; j++) {
+
+            //printing blocks
+            for (int j = 0; j < i + 2; j++)
+            {
                 printf("#");
             }
+
             printf("\n");
         }
 
