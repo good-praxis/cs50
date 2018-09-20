da2fdf6b58e7d005131a707007b350ec04ad51ba
Darian Reck <hello@vulpinecat.com>
2018-09-20 13:45:11 +0000

    change input method and syntax structure

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
+#include <cs50.h>
 
-//As I am writing this code ahead of watching the lecture, I am using https://www.tutorialspoint.com/cprogramming to help me
-//with input methods and type conversion
 
 int main(void)
 {
+    // initilization
+    int get_valid_input(void);
+    void print_spaces(int);
+    void print_blocks(int);
 
-    while (1)
+    int input = get_valid_input();
+
+    // This iterates through every line
+    for (int i = 0; i < input; i++)
     {
-
-        int startValue = -1;
-        int *input = &startValue;
-        //This input is the easiest way I found it to be done, will see if this is how they want us to do it after the lecture (I don't think so)
-
-        //Leaving loop when the value is permitted
-        while ((*input < 0) || (*input > 23))
-        {
-            printf("\nHeight: ");
-            scanf("%d", input);
-        }
-
-        //entering print loop
-        for (int i = 0; i < *input; i++)
-        {
-            //printing empty space
-            for (int j = 0; (j < (*input - 1 - i)); j++)
-            {
-                printf(" ");
-            }
-
-            //printing blocks
-            for (int j = 0; j < i + 2; j++)
-            {
-                printf("#");
-            }
-
-            printf("\n");
-        }
-
-        break;
+        print_spaces(input - i - 1);
+        print_blocks(i + 2);
+        printf("\n");
     }
+}
 
+// Gets an input that is and int between 0 and 23
+int get_valid_input(void)
+{
+    while (true)
+    {
+        int input = get_int("Height: ");
+        if (input >= 0 && input < 24)
+        {
+            return input;
+        }
+    }
+}
+
+// prints numberOfSpaces amount of spaces
+void print_spaces(int numberOfSpaces)
+{
+    for (int i = 0; i < numberOfSpaces; i++)
+    {
+        printf(" ");
+    }
+    return;
+}
+
+// prints numberOfBlocks amount of blocks (#)
+void print_blocks(int numberOfBlocks)
+{
+    for (int i = 0; i < numberOfBlocks; i++)
+    {
+        printf("#");
+    }
+    return;
 }
 
 
